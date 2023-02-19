53f1c69e75fc4fc3a23cb2ac2ec7bbef88359d37
Arian Sadeghi <arian.sadeghi03@outlook.com>
2023-02-19 17:55:42 -0500

    Improved the font, sizing, and other styling

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
         <meta name="description" content="Arian Sadeghi's personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet"/>
       </Head>
       <main className={styles.main}>
         <NavBar/>
